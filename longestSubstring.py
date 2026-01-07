b3629598e3e7b0a1c5f6c55812507320228c5e7f
hectorkennerley <121647461+hectorkennerley@users.noreply.github.com>
2026-01-07 14:49:32 +0000

    Optimize lengthOfLongestSubstring with two pointers
    Refactor lengthOfLongestSubstring method to use two pointers for improved efficiency.

--- longestSubstring.py
+++ longestSubstring.py
@@ -1,8 +1,16 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max_len = 0
-        for i in range(0,len(s)):
-            for j in range(0,len(s)):
-                if len(set(s[i:j])) == len(s[i:j]) and len(s[i:j]) > max_len:
-                    max_len = len(s[i:j])
-        return max_len
+        maxLen = 0
+        lPointer = 0
+        rPointer = 0
+        while lPointer < len(s):
+            while rPointer <= len(s):
+                if len(set(s[lPointer:rPointer])) == len(s[lPointer:rPointer]):
+                    maxLen = max(maxLen, len(s[lPointer:rPointer]))
+                    rPointer += 1
+                else:
+                    lPointer += 1
+                    rPointer = lPointer
+            lPointer += 1
+            rPointer = lPointer
+        return maxLen
